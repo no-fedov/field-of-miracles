141e6b49423ff6806f91d1e97e8ec1dd9b1f81c7
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-07-25 21:50:31 +0300

    feature: make 4 stage

--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,7 @@
 import org.javaacademy.wonder_field.Game;
-import org.javaacademy.wonder_field.Tableau;
 
 public class Runner {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Game game = new Game();
     }
 }
